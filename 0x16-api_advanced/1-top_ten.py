200aae0f82c353276616d4c697e50ddfbe8b7043
Kimberley-saka <kimberleysaka@gmail.com>
2023-05-09 07:24:15 -0700

    solution to tsak 2

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Lists 10 hot posts under given subreddit
+
+"""
+import requests
+
+
+def top_ten(subreddit):
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {"User-Agent": "my-app/0.0.1"}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        for post in data["data"]["children"]:
+            print(post["data"]["title"])
+    else:
+        print(None)
