9e24dcfaa6b10824be32ff4493ad88cf4c060edc
Kimberley-saka <kimberleysaka@gmail.com>
2023-05-09 07:29:41 -0700

    dont allow redirects

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -9,7 +9,7 @@ import requests
 def top_ten(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
     headers = {"User-Agent": "my-app/0.0.1"}
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
         for post in data["data"]["children"]:
