9c03612ad6acf89dfb53d51182168fd604d434b8
Kimberley-saka <kimberleysaka@gmail.com>
2023-05-09 07:04:46 -0700

    api

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+gets number of subscribers of a subreddit
+
+"""
+
+import requests
+import sys
+
+
+subreddit = sys.argv[1]
+
+
+def number_of_subscribers(subreddit):
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        return (data['data']['subscribers'])
+    else:
+        return 0
